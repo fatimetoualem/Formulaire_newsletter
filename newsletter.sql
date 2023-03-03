251b1f2db3b3bf3f38d2a03df0354375d103a184
Fatimletou El ALEM <fatimetoualem@gmail.com>
2023-03-03 17:31:57 +0100

    version final

--- newsletter.sql
+++ newsletter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 03, 2023 at 01:40 PM
+-- Generation Time: Mar 03, 2023 at 04:24 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -20,6 +20,69 @@ SET time_zone = "+00:00";
 --
 -- Database: `newsletter`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `checkbox`
+--
+
+CREATE TABLE `checkbox` (
+  `id` int NOT NULL,
+  `label_checkbox` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `checkbox`
+--
+
+INSERT INTO `checkbox` (`id`, `label_checkbox`) VALUES
+(8, 'Peinture'),
+(9, 'Sculpture'),
+(10, 'Photographie'),
+(11, 'Art contemporain'),
+(12, 'Films'),
+(13, 'Art numérique'),
+(14, 'Installations');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interest`
+--
+
+CREATE TABLE `interest` (
+  `id_subscribers` int NOT NULL,
+  `id_checkbox` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `interest`
+--
+
+INSERT INTO `interest` (`id_subscribers`, `id_checkbox`) VALUES
+(154, 13),
+(154, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `origines`
+--
+
+CREATE TABLE `origines` (
+  `id` int NOT NULL,
+  `origine_label` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `origines`
+--
+
+INSERT INTO `origines` (`id`, `origine_label`) VALUES
+(3, 'Un ami m’en a parlé'),
+(4, 'Recherche sur internet'),
+(5, 'Publicité dans un magazine');
 
 -- --------------------------------------------------------
 
@@ -52,15 +115,47 @@ INSERT INTO `subscribers` (`id`, `created_on`, `email`, `first_name`, `name`, `o
 --
 
 --
+-- Indexes for table `checkbox`
+--
+ALTER TABLE `checkbox`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `interest`
+--
+ALTER TABLE `interest`
+  ADD KEY `subscribers_id` (`id_subscribers`),
+  ADD KEY `checkbox_id` (`id_checkbox`);
+
+--
+-- Indexes for table `origines`
+--
+ALTER TABLE `origines`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `subscribers`
 --
 ALTER TABLE `subscribers`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
   ADD KEY `id_origine` (`origine_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `checkbox`
+--
+ALTER TABLE `checkbox`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `origines`
+--
+ALTER TABLE `origines`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `subscribers`
@@ -71,6 +166,13 @@ ALTER TABLE `subscribers`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `interest`
+--
+ALTER TABLE `interest`
+  ADD CONSTRAINT `checkbox_id` FOREIGN KEY (`id_checkbox`) REFERENCES `checkbox` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `subscribers_id` FOREIGN KEY (`id_subscribers`) REFERENCES `subscribers` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `subscribers`
